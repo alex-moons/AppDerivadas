5868889fa3c95c02c2fe8b304dfa37d63a3779f6
Patricio Santos <58954863+PatoSantos@users.noreply.github.com>
2023-05-09 11:31:42 -0600

    Creación de clases Fraction, Polynomial, Term

---
+++ Swift/Clases/Polynomial.swift
@@ -0,0 +1,51 @@
+class Polynomial {
+  //Un polinomio tiene una lista de Términos con sus exponentes
+  var terms: [Term]
+  
+  init(terms: [Term]) {
+    self.terms = terms
+  }
+
+  //Funciones
+  func addTerm(_ term: Term) {
+    self.terms.append(term)
+  }
+
+  func order() {
+    //Los Términos se ordenan de mayor exponente a menor
+    terms.sort(by: >)
+  }
+  
+  func differentiate() -> Polynomial {
+    var terms: [Term] = []
+    for i in 0..<self.terms.count {
+      if (self.terms[i].exponent.numerator != 0){
+        terms.append(Term.differentiate(term: self.terms[i]))
+      }
+    }
+    return Polynomial(terms: terms)
+  }
+
+  //Funciones de output
+  func toString() -> String {
+    var str = ""
+    for i in 0..<self.terms.count {
+      if self.terms[i].coefficient.numerator == 0 {
+            continue
+        }
+      if (i != 0 && self.terms[i].coefficient.isPositive()){
+        str += "+"
+      }
+
+      str += self.terms[i].toString()
+    }
+    if str.isEmpty {
+        str = "0"
+    }
+    return str
+  }
+  
+  func toLatex() -> String {
+    return ""
+  }
+}
---
+++ Swift/Clases/Term.swift
@@ -0,0 +1,48 @@
+class Term {
+  //Los Términos tienen un coeficiente y un exponente de tipo Fracción
+  let coefficient: Fraction
+  let exponent: Fraction
+
+  init(coefficient: Fraction, exponent: Fraction) {
+    self.coefficient = coefficient
+    self.exponent = exponent
+  }
+
+  //differentiate() regresa el Término derivado
+  static func differentiate(term: Term) -> Term {
+    let newCoefficient = term.coefficient * term.exponent
+    let newExponent = term.exponent - Fraction(numerator: 1, denominator: 1)
+    return Term(coefficient: newCoefficient, exponent: newExponent)
+  }
+
+  //Comparadores
+  //La comparación de Términos se hace en base al exponente, no al coeficiente
+  //Un Término es mayor a otro si su exponente es mayor en valor
+  static func <(_ first: Term, _ second: Term) -> Bool {
+    return first.exponent < second.exponent
+  }
+
+  static func >(_ first: Term, _ second: Term) -> Bool {
+    return first.exponent > second.exponent
+  }
+
+  static func ==(_ first: Term, _ second: Term) -> Bool {
+    return first.exponent == second.exponent
+  }
+
+  //Funciones de output
+  func toString() -> String {
+    var result = ""
+    result += self.coefficient.toString() 
+    if (self.exponent.numerator == 1 && self.exponent.denominator == 1){
+      result += "x"
+    } else if (self.exponent.numerator != 0){
+      result += "x^" + self.exponent.toString()
+    }
+    return result
+  }
+
+  func toLatex() -> String {
+    return "" 
+  }
+}
